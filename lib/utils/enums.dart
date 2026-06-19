e9f3b884f43a9ef1fecca8e04ba7b16d0f27a08d
WALEED AHMED <waleed.allshore@gmail.com>
2026-06-20 00:02:16 +0500

    feat: Update API base URLs for production and QA environments

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -51,10 +51,8 @@ enum Endpoint {
 }
 
 enum BaseUrls {
-  // api('http://3.128.27.193/api/');
   api('https://api.skinsyncai.com/api/'),
   apiQa('https://api-qa.skinsyncai.com/api/');
-  // api('https://s21hn0m8-8084.asse.devtunnels.ms/api/');
 
   final String url;
 
