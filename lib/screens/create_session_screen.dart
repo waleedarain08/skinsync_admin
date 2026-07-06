09c1a26a5e8f62ce150bfb1e42bb32340fba38aa
WALEED AHMED <waleed.allshore@gmail.com>
2026-07-06 23:29:29 +0500

    feat: update session screen to fetch products by treatment category on init

--- lib/screens/create_session_screen.dart
+++ lib/screens/create_session_screen.dart
@@ -32,15 +32,9 @@ class _CreateTreatmentScreenState extends ConsumerState<CreateSessionScreen> {
   @override
   void initState() {
     super.initState();
-    // WidgetsBinding.instance.addPostFrameCallback((_) async {
-    //   ref.read(categoryViewModelProvider.notifier).fetchCategories();
-    //   ref.read(productViewModelProvider.notifier).fetchProducts();
-    //   await ref.read(areaViewModelProvider.notifier).fetchAreas();
-    //   final fetchedAreas = ref.read(areaViewModelProvider).areas;
-    //   ref
-    //       .read(treatmentDataViewModelProvider.notifier)
-    //       .setAreasFromBackend(fetchedAreas);
-    // });
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      ref.read(sessionViewModelProvider.notifier).fetchProductsByTreatmentCategory();
+    });
   }
 
   int _getSessionOffsetStep(int sessionStep) {
